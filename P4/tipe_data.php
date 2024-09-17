042f904dbf51052abfa72d3fa3c7586869f09507
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-17 09:00:18 +0700

    P4_Praktikum Bagian 2_Langkah11

--- P4/tipe_data.php
+++ P4/tipe_data.php
@@ -32,5 +32,14 @@ var_dump($apakahSiswaLulus);
 echo "<br>";
 var_dump($apakahSiswaSudahUjian);
 //
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
 
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "<p>Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
 ?>
