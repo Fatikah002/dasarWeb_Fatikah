f8c83200b1ba322133aa34d79daa08e933dbe5a0
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-17 09:22:47 +0700

    P4_Praktikum Bagian 3_Langkah 2

---
+++ P4/operator.php
@@ -0,0 +1,20 @@
+<?php 
+$a = 10;
+$b = 5;
+
+$hasilTambah    = $a + $b;
+$hasilKurang    = $a - $b;
+$hasilKali      = $a * $b;
+$hasilBagi      = $a / $b;
+$sisaBagi       = $a % $b;
+$pangkat        = $a ** $b;
+
+echo "Variabel a: {$a} <br> Variabel b: {$b} ";
+echo "<br>Hasil tambah       : " .$hasilTambah;
+echo "<br>Hasil kurang   : " .$hasilKurang;
+echo "<br>Hasil kali     : " .$hasilKali;
+echo "<br>Hasil bagi     : " .$hasilBagi;
+echo "<br>Sisa bagi      : " .$sisaBagi;
+echo "<br>Pangkat        : " .$pangkat;
+
+?>
