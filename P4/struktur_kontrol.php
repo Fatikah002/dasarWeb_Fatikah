823e30bebe972d9d0844cc3d7723a26c58f76c49
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-17 11:45:27 +0700

    P4_Praktikum Bagian 4_Langkah 6

--- P4/struktur_kontrol.php
+++ P4/struktur_kontrol.php
@@ -2,12 +2,25 @@
 $nilaiNumerik = 92;
 
 if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
-echo "Nilai huruf: A";
+    echo "Nilai huruf: A";
 } elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
-echo "Nilai huruf: B";
+    echo "Nilai huruf: B";
 } elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
-echo "Nilai huruf: C";
+    echo "Nilai huruf: C";
 } elseif ($nilaiNumerik < 70) {
-echo "Nilai huruf: D";
+    echo "Nilai huruf: D";
 }
+//
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "<br>Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+//
 ?>
