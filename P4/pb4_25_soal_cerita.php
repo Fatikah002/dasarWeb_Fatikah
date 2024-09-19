03f4ed14ae54097a2c6f481e238a209e4bdf3de2
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-19 16:35:21 +0700

    P4_Praktikum Bagian 4 _Langkah 25_Soal Cerita

---
+++ P4/pb4_25_soal_cerita.php
@@ -0,0 +1,8 @@
+<?php
+$poin = 500;
+$hadiah = ($poin > 500)?"YA" : "TIDAK";
+echo "========== Hitung Total Skor ===========";
+echo "<br>Total Skor Pemain: $poin";
+echo "<br> Apakah pemain mendapatkan hadiah? $hadiah ";
+
+?>
