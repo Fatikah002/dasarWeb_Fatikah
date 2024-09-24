42891a3d2a721f8c375fde4f9f922de00662339f
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-24 07:35:41 +0700

    P5_PHP2_Fungsi yang Mengembalikan Nilai

--- P5/fungsi.php
+++ P5/fungsi.php
@@ -24,18 +24,26 @@
     // perkenalan($saya, $ucapanSalam);
 
 //Fungsi dengan Nilai Default
-    //membuat fungsi
-    function perkenalan($nama, $salam="Assalamualaikum"){
-        echo $salam.",";
-        echo "Perkenalkan, nama saya ".$nama."<br/>";
-        echo "Senang berkenalan dengan Anda<br/>";
-    }
-    //memanggil fungsi yang sudah dibuat
-    perkenalan("Fatikah", "Hallo");
-    echo "<hr>";
-    $saya = "Fatikah";
-    $ucapanSalam = "Selamat Pagi";
-    //memanggil lagi tanpa mengisi parameter salam
-    perkenalan($saya);
+    // //membuat fungsi
+    // function perkenalan($nama, $salam="Assalamualaikum"){
+    //     echo $salam.",";
+    //     echo "Perkenalkan, nama saya ".$nama."<br/>";
+    //     echo "Senang berkenalan dengan Anda<br/>";
+    // }
+    // //memanggil fungsi yang sudah dibuat
+    // perkenalan("Fatikah", "Hallo");
+    // echo "<hr>";
+    // $saya = "Fatikah";
+    // $ucapanSalam = "Selamat Pagi";
+    // //memanggil lagi tanpa mengisi parameter salam
+    // perkenalan($saya);
 
+//Fungsi yang Mengembalikan Nilai
+    //membuat fungsi
+    function hitungUmur($thn_lahir, $thn_sekarang){
+        $umur =$thn_sekarang - $thn_lahir;
+        return $umur;
+    }
+    echo "Umur saya adalah ". hitungUmur(2005, 2024) ." tahun";
+    
 ?>
