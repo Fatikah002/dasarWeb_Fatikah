1a2121f0936ecf978b0c956056cc60d1e0d5dbe9
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-24 07:27:35 +0700

    P5_PHP2_Parameter dengan Nilai Default_Langkah 7

--- P5/fungsi.php
+++ P5/fungsi.php
@@ -1,27 +1,41 @@
 <?php
-// function perkenalan(){
-//     echo "Assalamualaikum, ";
-//     echo "Perkenalkan, nama saya Fatikah<br/>";
-//     echo "Senang berkenalan dengan Anda<br/>";
-// } 
+//FUNGSI
+    // function perkenalan(){
+    //     echo "Assalamualaikum, ";
+    //     echo "Perkenalkan, nama saya Fatikah<br/>";
+    //     echo "Senang berkenalan dengan Anda<br/>";
+    // } 
+    // //memanggil fungsi yang sudah dibuat
+    // perkenalan();
+    // perkenalan();
 
-// //memanggil fungsi yang sudah dibuat
-// perkenalan();
-// perkenalan();
+//Fungsi dengan Parameter
+    // function perkenalan($nama, $salam){
+    //     echo $salam.", ";
+    //     echo "Perkenalkan, nama saya ".$nama."<br/>";
+    //     echo "Senang berkenalan dengan Anda<br/>";
+    // }
+    // //memanggil fungsi yang sudah dibuat
+    // perkenalan("Fatikah", "Hallo");
+    // echo "<hr>";
+    // $saya = "Fatikah";
+    // $ucapanSalam = "Selamat Pagi";
+    // //memanggil lagi
+    // perkenalan($saya, $ucapanSalam);
 
-function perkenalan($nama, $salam){
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
-}
+//Fungsi dengan Nilai Default
+    //membuat fungsi
+    function perkenalan($nama, $salam="Assalamualaikum"){
+        echo $salam.",";
+        echo "Perkenalkan, nama saya ".$nama."<br/>";
+        echo "Senang berkenalan dengan Anda<br/>";
+    }
+    //memanggil fungsi yang sudah dibuat
+    perkenalan("Fatikah", "Hallo");
+    echo "<hr>";
+    $saya = "Fatikah";
+    $ucapanSalam = "Selamat Pagi";
+    //memanggil lagi tanpa mengisi parameter salam
+    perkenalan($saya);
 
-//memanggil fungsi yang sudah dibuat
-perkenalan("Fatikah", "Hallo");
-echo "<hr>";
-
-$saya = "Fatikah";
-$ucapanSalam = "Selamat Pagi";
-
-//memanggil lagi
-perkenalan($saya, $ucapanSalam);
 ?>
