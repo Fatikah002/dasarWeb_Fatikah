61a332977dd71c5e63a8b40ac8b05b3ef7c29001
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-09-24 07:18:50 +0700

    P5_PHP2_Fungsi dengan Parameter_Langkah 4

--- P5/fungsi.php
+++ P5/fungsi.php
@@ -1,11 +1,27 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Fatikah<br/>";
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Fatikah<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
+// } 
+
+// //memanggil fungsi yang sudah dibuat
+// perkenalan();
+// perkenalan();
+
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
-} 
+}
 
 //memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan("Fatikah", "Hallo");
+echo "<hr>";
+
+$saya = "Fatikah";
+$ucapanSalam = "Selamat Pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
