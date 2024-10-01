e892c14b0faea67fb8aa3fe7f747fffb15f8c661
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-01 08:40:24 +0700

    P6_Praktikum Bagian 3_Fungsi_Langkah 13

--- P6/praktik.php
+++ P6/praktik.php
@@ -38,11 +38,27 @@
 
 //Fungsi yang Mengembalikan Nilai
     //membuat fungsi
+    // function hitungUmur($thn_lahir, $thn_sekarang){
+    //     $umur =$thn_sekarang - $thn_lahir;
+    //     return $umur;
+    // }
+    // echo "Umur saya adalah ". hitungUmur(2005, 2024) ." tahun";
+
+//Memanggil Fungsi di Dalam Fungsi
+    //membuat fungsi
     function hitungUmur($thn_lahir, $thn_sekarang){
         $umur =$thn_sekarang - $thn_lahir;
         return $umur;
     }
-    echo "Umur saya adalah ". hitungUmur(2005, 2024) ." tahun";
+    function perkenalan($nama, $salam="Assalamualaikum"){
+        echo $salam.",";
+        echo "Perkenalkan, nama saya ".$nama."<br/>";
+        //memanggil fungsi lain
+        echo "Saya berusia ". hitungUmur(2005, 2024) ." tahun<br/>";
+        echo "Senang berkenalan dengan Anda<br/>";
+    }
+    //memanggil fungsi perkenalan
+        perkenalan("Fatikah");
 
 
 ?>
