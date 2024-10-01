8661ba6d2858abf59da072e6ec373ccde3657efb
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-01 08:34:22 +0700

    P6_Praktikum Bagian 3_Fungsi dengan Parameter_Langkah 4

--- P6/praktik.php
+++ P6/praktik.php
@@ -1,9 +1,25 @@
 <?php 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Fatikah<br/>";
-    echo "Senang berkenalan dengan kalian<br/>";
-}
-perkenalan();
-perkenalan();
+// function perkenalan(){
+//     echo "Assalamualaikum, ";
+//     echo "Perkenalkan, nama saya Fatikah<br/>";
+//     echo "Senang berkenalan dengan kalian<br/>";
+// }
+// perkenalan();
+// perkenalan();
+
+// Fungsi dengan Parameter
+    function perkenalan($nama, $salam){
+        echo $salam.", ";
+        echo "Perkenalkan, nama saya ".$nama."<br/>";
+        echo "Senang berkenalan dengan Anda<br/>";
+    }
+    //memanggil fungsi yang sudah dibuat
+    perkenalan("Fatikah", "Hallo");
+    echo "<hr>";
+    $saya = "Fatikah";
+    $ucapanSalam = "Selamat Pagi";
+    //memanggil lagi
+    perkenalan($saya, $ucapanSalam);
+
+
 ?>
