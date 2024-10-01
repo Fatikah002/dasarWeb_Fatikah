bd0abfa27b056a098a1fa3e7a414ee099e2e1180
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-01 08:29:55 +0700

    P6_Praktikum Bagian 3_Fungsi_Langkah 1

---
+++ P6/praktik.php
@@ -0,0 +1,9 @@
+<?php 
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Fatikah<br/>";
+    echo "Senang berkenalan dengan kalian<br/>";
+}
+perkenalan();
+perkenalan();
+?>
