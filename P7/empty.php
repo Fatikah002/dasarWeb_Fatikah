ad3da264d8bbd8477c9b5f9d293acb03c8daead0
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-08 07:22:17 +0700

    P7_Praktikum 2_Langkah 4

--- P7/empty.php
+++ P7/empty.php
@@ -5,4 +5,10 @@ if (empty($myArray)) {
 } else {
     echo "Array terdefinisi dan tidak kosong.";
 }
+
+if (empty($nonExistentVar)) {
+    echo "<br>Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}
 ?>
