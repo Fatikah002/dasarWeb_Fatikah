4cb81dd7040dcd982c597f2a4c8726642fd32737
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-12 16:30:05 +0700

    P7_Praktikum 5_Langkah 2, 6, 10

---
+++ P7/regex.php
@@ -0,0 +1,27 @@
+<?php 
+// Langkah 2
+// $pattern = '/[a-z]/'; //Cocokkan huruf kecil.
+// $text = 'This is a Sample Text.';
+
+// if (preg_match($pattern, $text)) {
+//     echo "Huruf kecil ditemukan";
+// } else {
+//     echo "Tidak ada huruf kecil";
+// }
+
+//Langkah 6
+// $pattern = '/[0-9]/'; //Cocokkan satu atau lebih digit.
+// $text = 'There are 123 apples.';
+// if (preg_match($pattern, $text, $matches)) {
+//     echo "Cocokkan: " . $matches[0];
+// } else {
+//     echo "Tidak ada yang cocok!";
+// }
+
+//Langkah 10
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = "I like apple pie.";
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text; //Output: "I like banana pie."
+?>
