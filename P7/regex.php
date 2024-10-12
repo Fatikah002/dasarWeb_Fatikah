91a768a74592b167f8510ec0afd003dd158a2cef
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-10-12 17:22:38 +0700

    P7_Praktikum 5_Langkah 14

--- P7/regex.php
+++ P7/regex.php
@@ -19,9 +19,18 @@
 // }
 
 //Langkah 10
-$pattern = '/apple/';
-$replacement = 'banana';
-$text = "I like apple pie.";
-$new_text = preg_replace($pattern, $replacement, $text);
-echo $new_text; //Output: "I like banana pie."
+// $pattern = '/apple/';
+// $replacement = 'banana';
+// $text = "I like apple pie.";
+// $new_text = preg_replace($pattern, $replacement, $text);
+// echo $new_text; //Output: "I like banana pie."
+
+//Langkah14
+$pattern = '/go*d/' ; // Cocokkan "god", "good", "gooood", dll.
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)){
+    echo "Cocokkan: " . $matches[0];
+} else{
+    echo "Tidak ada yang cocok!";
+}
 ?>
