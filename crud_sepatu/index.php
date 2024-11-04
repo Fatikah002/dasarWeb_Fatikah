294107bc6a9f497e9c20aa8ee8d0e124a1e8a69e
Fatikah Salsabilla <sfatikah2005@gmail.com>
2024-11-04 15:28:44 +0700

    ubah

--- crud_sepatu/index.php
+++ crud_sepatu/index.php
@@ -95,7 +95,7 @@ if ($sql) {
             </table>
         </section>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
